8e1f14ca9fe5e6eca8813e207f832bdf3dbfa020
Mikkel Lau <mikkellau98@hotmail.com>
2023-09-21 10:41:47 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lau-tours',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
